01afb895243c27aadd99c2a7022ddf57c40cd9d1
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-16 19:39:34 -0600

    Update: responsive design fixed bugs

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 bg-gray-100 antialiased">
         <div class="flex flex-col sm:flex-row h-screen justify-center">
-            <div class="w-full sm:w-1/2 bg-cover bg-center" style="background-image:url('{{ asset('imgs/backgroun-biblioteca.jpg') }}');">
+            <div class="w-full sm:w-10/12 bg-cover bg-center" style="background-image:url('{{ asset('imgs/backgroun-biblioteca.jpg') }}');">
                 <a href="http://www.itsncg.edu.mx/"><img src="{{ asset('imgs/logo.png') }}" alt="Logo" class="hidden sm:block absolute bottom-0 left-3 w-16 h-30 p-2"></a>
             </div>
             <div class="w-full sm:w-1/2 flex flex-col justify-center items-center mx-auto">
