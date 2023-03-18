5d90584097be2f49d39776a6183f658e47d838b5
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-17 19:43:21 -0600

    Update: new configuration for dashboard admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-white">
-            <h2>Aqui estoy</h2>
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-            <h2 class="text-white">Aqui estoy</h2>
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white">
