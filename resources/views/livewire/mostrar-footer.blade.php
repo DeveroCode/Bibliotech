05a821f7eeee2b63a7324341bc759e296bbd7d07
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-17 21:33:33 -0600

    Update: we create the new footer with livewire

---
+++ resources/views/livewire/mostrar-footer.blade.php
@@ -0,0 +1,42 @@
+<div class="bg-indigo-800 text-gray-400">
+    <footer class="container mx-auto py-6 px-4">
+      <div class="flex flex-wrap justify-between md:text-start text-center">
+        <div class="w-full md:w-1/3 mb-4 md:mb-0"">
+            <h3 class="text-lg font-semibold mb-2">Contacto</h3>
+            <p>
+                Av. Tecnologico No. 7100 C.P. 31700
+            </p>
+            <p>
+                <i class="fa-solid fa-phone mr-1"></i> (636)-692-95-00 EXT. 110
+            </p>
+            <p>devero@devero.com</p>
+        </div>
+        <div class="w-full md:w-1/3 mb-4 md:mb-0">
+            <h3 class="text-lg font-semibold mb-2">Enlaces</h3>
+            <ul class="list-none">
+            <li><a href="#" class="hover:text-white transition duration-300 ease-in">Inicio</a></li>
+            <li><a href="http://www.itsncg.edu.mx/documentos/PROGRAMA_ANUAL_TRABAJO_COMITe_eTICA.pdf" target="_blank" class="hover:text-white transition duration-300 ease-in">Aviso de privacidad</a></li>
+            <li><a href="http://www.itsncg.edu.mx/servicios" target="_blank" class="hover:text-white transition duration-300 ease-in">Servicios</a></li>
+            <li><a href="http://www.itsncg.edu.mx/conocenos" target="_blank" class="hover:text-white transition duration-300 ease-in">Nosotros</a></li>
+            </ul>
+        </div>
+        <div class="w-full md:w-1/3 mb-4 md:mb-0">
+            <h3 class="text-lg font-semibold mb-2">Redes Sociales</h3>
+            <ul class="list-none">
+            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-facebook mr-1"></i>Facebook</a></li>
+            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-twitter mr-1"></i>Twitter</a></li>
+            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-instagram mr-1"></i>Instagram</a></li>
+            </ul>
+        </div>
+        <div class="text-center mt-6">
+          <p>&copy; 2023 <a href="https://github.com/CMartinez-BB/Bibliotech" target="_blank" class="hover:text-white transition duration-300 ease-in">CodignWhite</a>. Todos los derechos reservados.</p>
+        </div>
+      </div>
+    </footer>
+</div>
+
+{{-- Fontawesome script --}}
+@push('scripts')
+    <script src="https://kit.fontawesome.com/85d631ed4b.js" crossorigin="anonymous"></script>
+@endpush
+
