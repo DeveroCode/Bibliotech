e8f819c3b4fad839513ef6f89acc1c3229aabea1
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-17 22:05:49 -0600

    Update: hover fixed

--- resources/views/livewire/mostrar-footer.blade.php
+++ resources/views/livewire/mostrar-footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-indigo-800 text-gray-400">
+<div class="bg-indigo-900 text-white">
     <footer class="container mx-auto py-6 px-4">
       <div class="flex flex-wrap justify-between md:text-start text-center">
         <div class="w-full md:w-1/3 mb-4 md:mb-0"">
@@ -14,22 +14,22 @@
         <div class="w-full md:w-1/3 mb-4 md:mb-0">
             <h3 class="text-lg font-semibold mb-2">Enlaces</h3>
             <ul class="list-none">
-            <li><a href="#" class="hover:text-white transition duration-300 ease-in">Inicio</a></li>
-            <li><a href="http://www.itsncg.edu.mx/documentos/PROGRAMA_ANUAL_TRABAJO_COMITe_eTICA.pdf" target="_blank" class="hover:text-white transition duration-300 ease-in">Aviso de privacidad</a></li>
-            <li><a href="http://www.itsncg.edu.mx/servicios" target="_blank" class="hover:text-white transition duration-300 ease-in">Servicios</a></li>
-            <li><a href="http://www.itsncg.edu.mx/conocenos" target="_blank" class="hover:text-white transition duration-300 ease-in">Nosotros</a></li>
+            <li><a href="#" class="hover:text-gray-400 transition ease-out duration-300">Inicio</a></li>
+            <li><a href="http://www.itsncg.edu.mx/documentos/PROGRAMA_ANUAL_TRABAJO_COMITe_eTICA.pdf" target="_blank" class="hover:text-gray-400 transition ease-out duration-300">Aviso de privacidad</a></li>
+            <li><a href="http://www.itsncg.edu.mx/servicios" target="_blank" class="hover:text-gray-400 transition ease-out duration-300">Servicios</a></li>
+            <li><a href="http://www.itsncg.edu.mx/conocenos" target="_blank" class="hover:text-gray-400 transition ease-out duration-300">Nosotros</a></li>
             </ul>
         </div>
         <div class="w-full md:w-1/3 mb-4 md:mb-0">
             <h3 class="text-lg font-semibold mb-2">Redes Sociales</h3>
             <ul class="list-none">
-            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-facebook mr-1"></i>Facebook</a></li>
-            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-twitter mr-1"></i>Twitter</a></li>
-            <li><a href="#" class="hover:text-white transition duration-300 ease-in"><i class="fa-brands fa-instagram mr-1"></i>Instagram</a></li>
+            <li><a href="https://www.facebook.com/tecnmcampusnuevocasasgrandes/" target="_blank" class="hover:text-gray-400 transition ease-out duration-300"><i class="fa-brands fa-facebook mr-1"></i>Facebook</a></li>
+            <li><a href="#" target="_blank" class="hover:text-gray-400 transition ease-out duration-300"><i class="fa-brands fa-twitter mr-1"></i>Twitter</a></li>
+            <li><a href="https://www.instagram.com/tecnmcampusncg/" target="_blank" class="hover:text-gray-400 transition ease-out duration-300"><i class="fa-brands fa-instagram mr-1"></i>Instagram</a></li>
             </ul>
         </div>
         <div class="text-center mt-6">
-          <p>&copy; 2023 <a href="https://github.com/CMartinez-BB/Bibliotech" target="_blank" class="hover:text-white transition duration-300 ease-in">CodignWhite</a>. Todos los derechos reservados.</p>
+          <p>&copy; 2023 <a href="https://github.com/CMartinez-BB/Bibliotech" target="_blank" class="hover:text-gray-400 transition ease-out duration-300">CodignWhite</a>. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
