08b0abf8d0019427babcd444fd48591c8755718d
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-18 22:15:29 -0600

    fixed: fixed bugs

--- resources/views/administrator/create.blade.php
+++ resources/views/administrator/create.blade.php
@@ -1,30 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Panel administrador') }}
+            {{ __('Agregar nuevo libro') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                {{ '¡Bienvenido' . Auth::user()->name .' al panel de "Agregar Libro"! Esta sección te permite crear y gestionar tus libros en línea de una manera fácil y sencilla.'}}
-            </div>
+                <h2 class="mb-10 text-center text-sm mt-10 font-bold uppercase">{{ '¡Bienvenido ' . Auth::user()->name .' al panel de "Agregar Nuevo Libro"!. Esta sección te permite agregar nuevos libros en línea de una manera fácil y sencilla.'}}</h2>
 
-            {{-- Links --}}
-            <div class="p-6 ">
-                <div class="container w-1/3 mx-auto px-4 py-4 sm:px-6 lg:px-8">
-                    <div class="bg-sea-400 p-4 rounded-md">
-                        <p class="text-xl text-gray-800 uppercase font-bold mb-2">Agregar</p>
-                        <div class="flex justify-center">
-                        <a href="{{ route('dashboard.create') }}" class="text-blue-500 hover:text-blue-600 underline">Libro</a>
-                        </div>
-                    </div>
+                <div class="md:flex md:justify-center p-5 text-2xl">
+                    <livewire:crear-libro/>
                 </div>
             </div>
         </div>
-
-        @stack('scripts')
     </div>
 </x-app-layout>
 
