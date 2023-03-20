193725bc6fa278e47d7ca883968fb2b40e436fe5
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-19 21:16:40 -0600

    Update: alert when the librarian creates a new book

--- resources/views/administrator/index.blade.php
+++ resources/views/administrator/index.blade.php
@@ -7,6 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{-- Alert --}}
+            @if (session()->has('message'))
+                <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show">
+                    <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3 text-sm text-center">
+                        {{ session('message') }}
+                    </div>
+                </div>
+            @endif
+
             <div class="overflow-hidden sm:rounded-lg">
                 <div class="p-6 text-gray-900 text-lg">
                     {{ 'Bienvenido ' .Auth::user()->name }}
