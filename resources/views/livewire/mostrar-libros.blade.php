2714b200a191e107b85c1e71cb107b433a2313e0
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-03-31 22:20:32 -0600

    New: livewire component for the search bar

--- resources/views/livewire/mostrar-libros.blade.php
+++ resources/views/livewire/mostrar-libros.blade.php
@@ -2,19 +2,7 @@
 <div class="w-full">
 
     {{-- Bar seach --}}
-    <div class="flex justify-end items-end mb-10 w-1/2">
-        <input type="text" id="voice-search"
-            class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            placeholder="Buscar por ISBN" required="">
-        <button type="submit"
-            class="ml-2 inline-flex items-center py-2.5 px-3 text-sm font-medium text-white bg-indigo-700 border border-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 rounded-sm">
-            <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-            </svg>Buscar
-        </button>
-    </div>
+    <livewire:filtrar-isbn>
 
 
     <table class="table-auto text-xs w-full m-auto border-collapse bg-white text-left text-gray-500">
