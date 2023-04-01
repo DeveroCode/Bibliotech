a7b28c5126d3f7c9eda21a361d6058e6ab7c1296
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-01 13:36:03 -0600

    New: fixed busg in the new version of the program

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,15 +16,46 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('General') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
+                    {{-- Books and settigns --}}
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-1 pt-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white  hover:text-indigo-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Inventario</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
+                                    {{ __('Libros') }}
+                                </x-dropdown-link>
+
+                                {{-- Print reports --}}
+                                <x-dropdown-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
+                                    {{ __('Agregar') }}
+                                </x-dropdown-link>
+
+                                <x-dropdown-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
+                                    {{ __('Imprimir reportes') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                    {{-- <x-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
                         {{ __('Libros') }}
                     </x-nav-link>
                     <x-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
                         {{ __('Agregar') }}
                     </x-nav-link>
-                    <x-nav-link>
+                    <x-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
                         {{ __('Reportes') }}
-                    </x-nav-link>
+                    </x-nav-link> --}}
                 </div>
             </div>
 
@@ -51,13 +82,17 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Salir') }}
                             </x-dropdown-link>
                         </form>
+
+                        {{-- Print reports --}}
+                        <x-dropdown-link :href="route('dashboard.print')">
+                            {{ __('Imprimir reporte') }}
+                        </x-dropdown-link>
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -86,13 +121,13 @@
             <x-responsive-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
                 {{ __('Agregar') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
                 {{ __('Reportes') }}
             </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+        <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
