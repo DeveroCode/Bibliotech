7e6fb979c68780b56ff119bcf8b666b8fe15f0de
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-06 19:17:56 -0500

    Fixed bugs

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,103 +5,130 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" class="mt-3 mb-3">
+                    <a href="{{ route('home') }}" class="mt-3 mb-3">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                         <h2>ITSNCG</h2>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('General') }}
-                    </x-nav-link>
-                    {{-- Books and settigns --}}
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-1 pt-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white  hover:text-indigo-700 focus:outline-none transition ease-in-out duration-150">
-                                    <div>Inventario</div>
+                @auth
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('General') }}
+                        </x-nav-link>
+                        {{-- Books and settigns --}}
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button
+                                        class="inline-flex items-center px-1 pt-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white  hover:text-indigo-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>Inventario</div>
 
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                                        <div class="ml-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
 
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
-                                    {{ __('Libros') }}
-                                </x-dropdown-link>
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('dashboard.show')"
+                                        :active="request()->routeIs('dashboard.show')">
+                                        {{ __('Libros') }}
+                                    </x-dropdown-link>
 
-                                {{-- Print reports --}}
-                                <x-dropdown-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
-                                    {{ __('Agregar') }}
-                                </x-dropdown-link>
+                                    {{-- Print reports --}}
+                                    <x-dropdown-link :href="route('dashboard.create')"
+                                        :active="request()->routeIs('dashboard.create')">
+                                        {{ __('Agregar') }}
+                                    </x-dropdown-link>
 
-                                <x-dropdown-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
-                                    {{ __('Imprimir reportes') }}
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
+                                    <x-dropdown-link :href="route('dashboard.print')"
+                                        :active="request()->routeIs('dashboard.print')">
+                                        {{ __('Imprimir reportes') }}
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+
+                        <x-nav-link :href="route('dashboard.lending')" :active="request()->routeIs('dashboard.lending')">
+                            {{ __('Prestamos') }}
+                        </x-nav-link>
+                        {{-- <x-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
+                            {{ __('Libros') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
+                            {{ __('Agregar') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
+                            {{ __('Reportes') }}
+                        </x-nav-link> --}}
                     </div>
-
-                    <x-nav-link :href="route('dashboard.lending')" :active="request()->routeIs('dashboard.lending')">
-                        {{ __('Prestamos') }}
-                    </x-nav-link>
-                    {{-- <x-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
-                        {{ __('Libros') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
-                        {{ __('Agregar') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
-                        {{ __('Reportes') }}
-                    </x-nav-link> --}}
-                </div>
+                @endauth
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
 
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+            <div class="hidden space-x-8 sm:flex sm:items-center sm:ml-6">
+                @auth
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Salir') }}
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profile') }}
                             </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Salir') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @endauth
             </div>
+
+            @guest
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('login')">
+                        {{ __('Iniciar sesión') }}
+                    </x-nav-link>
+                </div>
+            @endguest
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-indigo-100 focus:outline-none focus:bg-indigo-200 focus:text-gray-500transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-indigo-100 focus:outline-none focus:bg-indigo-200 focus:text-gray-500transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -110,44 +137,52 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('General') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
-                {{ __('Libros') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
-                {{ __('Agregar') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
-                {{ __('Reportes') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                    {{ __('Perfil') }}
+        @guest
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                    {{ __('Iniciar sesión') }}
                 </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Salir') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
-        </div>
+        @endguest
+        @auth
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('General') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard.show')" :active="request()->routeIs('dashboard.show')">
+                    {{ __('Libros') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
+                    {{ __('Agregar') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard.print')" :active="request()->routeIs('dashboard.print')">
+                    {{ __('Reportes') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
+                        {{ __('Perfil') }}
+                    </x-responsive-nav-link>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Salir') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            </div>
+        @endauth
     </div>
 </nav>
