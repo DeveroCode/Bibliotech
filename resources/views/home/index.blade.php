b04ffcbfc9a9b8da2e3b8ea1524890caad8cfaca
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-17 10:40:09 -0600

    new: bugs fixed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,7 +22,7 @@
                 {{-- Barra de busqueda --}}
                 <div class="relative inset-0 bg-opacity-75 z-10 flex items-center">
                     <div class="flex mt-4 space-x-3 lg:mt-6">
-                        <a type="submit" href="{{ route('search.books') }}" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 uppercase">Iniciar busqueda</a>
+                        <a type="submit" href="{{ route('search.books') }}" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-inidgo-800 focus:ring-4 focus:ring-blue-300 uppercase">Iniciar busqueda</a>
                     </div>
                 </div>
             </div>
