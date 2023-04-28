ca81a089e161b84db59f49d697fce5f90ef0d91c
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-28 15:50:24 -0600

    New: Styles to M.G.T.I Lilia Margarita Mena Castillo

--- resources/views/livewire/prestamos-libros.blade.php
+++ resources/views/livewire/prestamos-libros.blade.php
@@ -72,41 +72,114 @@
         </div>
 
         {{-- form for librarian--}}
-        <form class="flex flex-row py-6 gap-10">
-            {{-- nombre --}}
-            <div class="relative z-0 w-1/3">
-                <input type="text" name="name_prestamista"
-                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                    placeholder=" " />
-                <label
-                    class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
-                    Nombre del prestamista
-                </label>
+        <form class="flex flex-col py-6 gap-10">
+            <div class="block-primary w-full flex flex-row gap-5">
+                {{-- nombre --}}
+                <div class=" relative z-0 w-1/3">
+                    <input type="text" name="name_prestamista"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Nombre del prestamista
+                    </label>
+                </div>
+                {{-- folio --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="name_prestamista"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        No. Folio
+                    </label>
+                </div>
+                {{-- date --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="fecha_prestamo"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " value="{{
+                            now()->format('d/m/Y') }}" />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Fecha
+                    </label>
+                </div>
             </div>
 
-            {{-- folio --}}
-            <div class="relative z-0 w-1/3">
-                <input type="text" name="name_prestamista"
-                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                    placeholder=" " />
-                <label
-                    class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
-                    No. Folio
-                </label>
+            <div class="block-secondary w-full flex flex-row gap-5">
+                {{-- Name Book --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="nombre_liro"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Libro
+                    </label>
+                </div>
+                {{-- Autores --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="autores"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Autores
+                    </label>
+                </div>
+                {{-- ISBN--}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="isbn"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        ISBN
+                    </label>
+                </div>
             </div>
 
-            {{-- date --}}
-            <div class="relative z-0 w-1/3">
-                <input type="text" name="fecha_prestamo"
-                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                    placeholder=" " value="{{
-                        now()->format('d/m/Y') }}" />
-                <label
-                    class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
-                    Fecha
-                </label>
+            <div class="block-trhee w-full flex flex-row gap-5">
+                {{-- No. adquisicion --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="no.adquisicion"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        No. adquisicon
+                    </label>
+                </div>
+
+                {{-- Renovacion --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="fecha_prestamo"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Renovacion
+                    </label>
+                </div>
+
+                {{-- Estatus --}}
+                <div class="relative z-0 w-1/3">
+                    <input type="text" name="Estatus"
+                        class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                        placeholder=" " />
+                    <label
+                        class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                        Estatus
+                    </label>
+                </div>
             </div>
         </form>
-    </div>
 
-</div>
+        <div class="pt-10 flex justify-end">
+            <input type="submit"
+                class="inline-flex uppercase items-center w-full px-5 py-2 mb-3 mr-1 text-sm font-bold text-white no-underline align-middle bg-indigo-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-indigo-700 hover:border-indigo-700 hover:text-white focus-within:bg-indigo-700 focus-within:border-blindigoue-700 text-center"
+                value="Enviar e Imprimir">
+        </div>
+    </div>
+</div>clear
