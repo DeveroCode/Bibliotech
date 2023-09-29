de6a11b5caf12ee55e8cd889758d6760888e73a8
DeveroCode <charli.rey.misterio@gmail.com>
2023-09-28 18:13:37 -0600

    New: we add the préstamo table for making prestamos

--- app/Http/Livewire/PrestamosLibros.php
+++ app/Http/Livewire/PrestamosLibros.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire;
 
 use App\Models\Alumno;
+use App\Models\Libro;
 use Livewire\Component;
 
 class PrestamosLibros extends Component
 {
     public $no_institucional;
+    public $isbn;
+
+    // Iniciar vació el array
     public $alumno = [];
-    protected $listeners = ['searchWord' => 'buscar'];
+    public $isb = [];
+    protected $listeners = ['searchWord' => 'buscar', 'leerDatos' => 'isb'];
     public function buscar($no_institucional)
     {
         $this->no_institucional = $no_institucional;
@@ -23,10 +28,23 @@ class PrestamosLibros extends Component
         $this->alumno = $alumno;
     }
 
+    public function isb($isbn)
+    {
+        $this->isbn = $isbn;
+
+        if (empty($this->isbn)) {
+            $this->isbn = [];
+            return;
+        };
+
+        $isbn = $this->isbn = Libro::where('isbn', 'LIKE', '%' . $this->isbn . '%')->get();
+        $this->isbn = $isbn;
+    }
     public function render()
     {
         return view('livewire.prestamos-libros', [
             'alumno' => $this->alumno,
+            'isbn' => $this->isbn,
         ]);
     }
 }
