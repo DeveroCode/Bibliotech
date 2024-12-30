606091129a64a94375b00fb48c992a9c86e0ee7d
CMartinez <devero@github.com>
2024-12-29 19:20:15 -0600

    fixed

--- app/Http/Livewire/SearchUser.php
+++ app/Http/Livewire/SearchUser.php
@@ -2,23 +2,27 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Alumno;
 use Livewire\Component;
 
 class SearchUser extends Component
 {
     public $no_institucional;
+    public $found = null;
+    public $listeners = ['status' => 'leerStatus'];
+
+    public function leerStatus($found)
+    {
+        $this->found = $found;
+    }
 
     public function leerDatosBuscar()
     {
         $this->emit('searchWord', $this->no_institucional);
     }
-
     public function render()
     {
-        $alumno = Alumno::all();
         return view('livewire.search-components.search-user', [
-            'alumno' => $alumno,
+            'found' => $this->found,
         ]);
     }
 }
