41babfcc9686fdef10347b40ea84cb69ab170a74
Devero <dever@code.com>
2025-06-08 20:56:25 -0700

    last details

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -22,24 +22,6 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-indigo-600">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002" />
-                                    </svg>
-                                </div>
-                                <div class="ml-4">
-                                    <h2 class="font-bold uppercase text-xl text-gray-500">Invitado</h2>
-                                    <p class="mt-2 text-sm text-gray-500">Los usuarios invitados tienen permisos para
-                                        realizar acciones básicas como ver contenido público, realizar búsquedas,
-                                        navegar por el sitio o la aplicación.</p>
-                                </div>
-                            </div>
-
-                            {{-- Second Card --}}
-                            <div class="flex items-start rounded-xl bg-white p-4 shadow-lg w-full sm:w-3/4 lg:w-1/2">
-                                <div
-                                    class="flex py-2 px-2 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-indigo-600">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                                     </svg>
                                 </div>
@@ -51,7 +33,7 @@
                                 </div>
                             </div>
 
-                            {{-- Third Card --}}
+                            {{-- Second Card --}}
                             <div class="flex items-start rounded-xl bg-white p-4 shadow-lg w-full md:w-3/4 lg:w-1/2">
                                 <div
                                     class="flex py-2 px-2 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
@@ -70,15 +52,16 @@
                                 </div>
                             </div>
                         </div>
-                    </section>
-
-                    @if ($editMode)
-                    <livewire:crear-usuario :user="$user" />
-                    @else
-                    <livewire:crear-usuario />
-                    @endif
                 </div>
+                </section>
+
+                @if ($editMode)
+                <livewire:crear-usuario :user="$user" />
+                @else
+                <livewire:crear-usuario />
+                @endif
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
